e5dcab8d0185dd59ef702f942c55bc786c227396
Trevor Luttrell <trevorluttrell1492@hotmail.com>
2025-12-03 19:32:09 -0600

    Added level 5 and ending.

---
+++ real-time-game/Scripts/Level5.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class Level5 : LevelBase
+{
+	public override void NextLevel()
+	{
+		CallDeferred(nameof(LoadNextLevel));
+	}
+
+	private void LoadNextLevel()
+	{
+		var nextScene = ResourceLoader.Load<PackedScene>("res://scenes/level_win.tscn");
+		GetTree().ChangeSceneToPacked(nextScene);
+	}
+}
